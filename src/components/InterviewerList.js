3787a9d7e65668fe6cca7697f48608f9cc76a456
Karl Eisen Yabut <karleisenyabut@gmail.com>
2019-10-01 20:49:47 -0400

    Finished Form section

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -14,5 +14,10 @@ export default function InterviewerList(props) {
       />
     );
   })
-  return interviewers;
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewers}</ul>
+    </section>
+  );
 }
--- stories/index.js
+++ stories/index.js
@@ -20,6 +20,8 @@ import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
 
+import Form from "components/Appointment/Form";
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -165,4 +167,16 @@ storiesOf("Button", module)
         .add("Error Deleting", () => <Error 
         message={"Could not delete appointment"}
         onClose={action("onClose")}
+        />)
+        .add("Create", () => <Form 
+        interviewers={interviewers}
+        onSave={action("onSave")}
+        onCancel={action("onCancel")}
+        />)
+        .add("Edit", () => <Form
+        name={"Eddie Man"}
+        interviewers={interviewers}
+        interviewer={3}
+        onSave={action("onSave")}
+        onCancel={action("onCancel")}
         />)
