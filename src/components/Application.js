8337b1529e1d2775a0fda892ed1a2d5884162fb2
Karl Eisen Yabut <karleisenyabut@gmail.com>
2019-10-02 20:53:11 -0400

    Used Promise.all for API calls for days/appointment (NEED TO USE useEffect)

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,6 +4,7 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment/index";
 import axios from "axios";
+import { Promise } from "q";
 
 const appointments = [
   {
@@ -87,14 +88,31 @@ export default function Application(props) {
     appointments: {}
   })
   const setDay = day => setState({...state, day});
-  const setDays = days => setState(prev => ({ ...prev, days }));
+  // // const setDays = days => setState(prev => ({ ...prev, days }));
 
-  //GET Days
-  useEffect(() => {
-    axios.get("/api/days").then((response) => {
-      setDays(response.data)
-    });
-  }, [])
+  // // //GET Days
+  // // useEffect(() => {
+  // //   axios.get("/api/days").then((response) => {
+  // //     setDays(response.data)
+  // //   });
+  // // }, [])
+
+  //API CALLS
+  Promise.all([
+    Promise.resolve(
+      axios.get("/api/days")
+    ),
+    Promise.resolve(
+      axios.get("/api/appointments")
+    )
+  ]).then((all) => {
+    console.log(all);
+    setState(prev => ({
+      day: "Monday",
+      days: all[0].data,
+      appointments: all[1].data
+    }))
+  });
 
   const appointmentList = appointments.map(appointment => {
     return (
